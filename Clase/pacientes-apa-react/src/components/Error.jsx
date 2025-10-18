1c015b09575c2f57f525dbc7a264c4ec622d014c
MatiasNavone <matiasnavone@hotmail.com>
2025-10-17 21:29:35 -0300

    actualizacion-pacientes

--- Clase/pacientes-apa-react/src/components/Error.jsx
+++ Clase/pacientes-apa-react/src/components/Error.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
-const Error = () => {
+const Error = ({children}) => {
   return (
-    <div>
-      
-    </div>
+      <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+          {children}
+      </div>
   )
 }
 
